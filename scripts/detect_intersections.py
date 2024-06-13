2e23bd669b1e97c4919320f9e17384782c935d50
Eric Tran <extran@gatech.edu>
2024-06-13 13:52:39 -0400

    Deleted unncessary files, added gene duplication data.

---
+++ scripts/detect_intersections.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+The following script takes as input a GFF file, BLASTp output file in FMT 6, 
+and a MobileElementFinder results file. It detects if the BLAST protein hits
+reside inside or outside an MGE.
+"""
+
+
+def main():
+    print("Ello Wolrd.")
